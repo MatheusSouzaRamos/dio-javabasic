c9bb3fdd10ea5fbe3f8979eb999b76adaa9e446f
MatheusSouzaRamos <strikman7@gmail.com>
2024-05-01 20:02:59 -0300

    tipos-variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        byte a;
+        short b;
+        int c;
+        long d = 111L;
+        float e = 1.234f;
+        double f;
+        final float PI = 3.14f;
+    }
+}
