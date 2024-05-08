2c84e8aac6d2aa76df1254d2cc07457662fe100b
MatheusSouzaRamos <strikman7@gmail.com>
2024-05-08 17:34:32 -0300

    encapsulamento

---
+++ POO2/src/ComputadordePedrinho.java
@@ -0,0 +1,9 @@
+public class ComputadordePedrinho {
+    public static void main(String[] args) {
+        MSNMensager msn = new MSNMensager();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+    
+}
